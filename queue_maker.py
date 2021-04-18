3156f468f012fa711f23f02f153059a8c18c5f95
Rajarshi Roy <rajarshi008@gmail.com>
2021-04-18 22:55:58 +0200

    small fix to output csv files

--- queue_maker.py
+++ queue_maker.py
@@ -201,7 +201,7 @@ def get_parser(parser=None):
 
 	group_multiproc.add_argument("-f", "--traces_folder",
 	    dest='traces_folder',
-	    default="../all_traces/",
+	    default="../all_traces_maxsat/",
 		help="trace file/folder to run",
 	)
 	group_multiproc.add_argument("-T", "--timeout", metavar="T",
